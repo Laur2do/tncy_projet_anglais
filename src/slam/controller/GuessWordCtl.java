2aafa5a28e297851ae5620822a53dfa3f39ab201
Laury de Donato <laury.de-donato@telecomnancy.eu>
2019-03-05 23:06:50 +0100

    Amélioration de l'interface graphique

--- src/slam/controller/GuessWordCtl.java
+++ src/slam/controller/GuessWordCtl.java
@@ -3,9 +3,10 @@ package slam.controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
-import slam.model.Cell;
+import javafx.scene.control.TextFormatter;
 import slam.model.GridWord;
+
+import java.util.function.UnaryOperator;
 
 import static slam.Main.DEBUG;
 
@@ -17,39 +18,44 @@ public class GuessWordCtl {
     private Label definition;
 
     @FXML
-    private Label word;
-
-    @FXML
     private TextField answer;
 
-    public GuessWordCtl() {}
+    public GuessWordCtl() {
+    }
 
     public void setGridWord(GridWord gw) {
         this.gridWord = gw;
-        StringBuilder sb = new StringBuilder();
 
-        boolean[] revealedLetters = gw.getRevealedLetters();
-        for (int i = 0; i < gw.getLength(); i++) {
-            if (revealedLetters[i]) {
-                sb.append(gw.getLetter(i));
-            } else {
-                sb.append(Cell.NOT_REVEALED_LETTER_CHAR);
+        int wordLength = this.gridWord.getLength();
+        this.definition.setText(gw.getDefinitions());
+
+        System.out.println("You picked " + gw.toString());
+
+        this.answer.setPrefColumnCount(wordLength);
+        this.answer.setMaxWidth(wordLength * this.answer.getFont().getSize());
+
+        UnaryOperator<TextFormatter.Change> modifyChange = c -> {
+            if (c.isContentChange()) {
+                if (c.getControlNewText().length() > wordLength) {
+                    // Trim the text to the first char
+                    c.setText(c.getControlNewText().substring(0, wordLength));
+                    // Set the range of the change from 0 to all the new added text
+                    c.setRange(0, c.getControlText().length());
+                }
             }
-        }
-        sb.append("\n");
-        word.setText(sb.toString());
-
-        definition.setText(gw.getDefinitions());
+            return c;
+        };
+        this.answer.setTextFormatter(new TextFormatter(modifyChange));
     }
 
     public void validate() {
-        if (this.gridWord.getContent().equals(answer.getText().toUpperCase())) {
+        if (this.gridWord.getContent().equals(this.answer.getText().toUpperCase())) {
             if (DEBUG) {
-                System.out.println("> " + this.answer.getText()+"\nCorrect!");
+                System.out.println("> " + this.answer.getText() + "\nCorrect!");
             }
             this.gridWord.reveal();
         } else if (DEBUG) {
-            System.out.println("> " + this.answer.getText()+"\nIncorrect!");
+            System.out.println("> " + this.answer.getText() + "\nIncorrect!");
         }
     }
 }
